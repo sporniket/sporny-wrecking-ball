5044902e01e6867dc91ac6277e91719ea6384807
David SPORN <sporniket.studio@gmail.com>
2022-05-22 21:16:14 +0200

    basic setup, environment also source a local part

--- zz_tools/ap_cli/ap.bash
+++ zz_tools/ap_cli/ap.bash
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
-echo $(pwd)
 AP_WORKDIR=$(cd $(dirname $0) && pwd)
 
-echo $AP_WORKDIR
-echo $(pwd)
 ap_help() {
   cli_name=${0##*/}
   echo -e "
 \e[90m$cli_name\e[0m
-\e[93map\e[96m, the \e[93mA\e[96mssembly \e[93mP\e[96mrogram CLI -- version $(cat $AP_WORKDIR/VERSION)\e[0m
+\e[90m---===<{\e[93map\e[96m, the \e[93mA\e[96mssembly \e[93mP\e[96mrogram CLI -- version $(cat $AP_WORKDIR/VERSION)\e[90m}>===---\e[0m
 
 \e[96mUsage:\e[0m $cli_name \e[93m[command]\e[0m
 \e[96mCommands:\e[0m
-  \e[93msetup\e[0m     Setup utility
+  \e[93msetup\e[0m     CLI Setup utility
+  \e[93mbuild\e[0m     CLI Build command
+  \e[93mrun\e[0m       CLI program launcher
   \e[93m*\e[0m         Help
 "
   exit 1
@@ -20,7 +19,13 @@ ap_help() {
 
 case "$1" in
   setup)
-    "$AP_WORKDIR/commands/setup_wizz.bash" "$2"
+    "$AP_WORKDIR/commands/setup_wizz.bash" "$(pwd)"
+    ;;
+  build)
+    "$AP_WORKDIR/commands/build.bash" "${2}"
+    ;;
+  run)
+    "$AP_WORKDIR/commands/run.bash" "${2}"
     ;;
   *)
     ap_help
--- zz_tools/ap_cli/commands/setup_wizz.bash
+++ zz_tools/ap_cli/commands/setup_wizz.bash
@@ -1,20 +1,28 @@
 #!/usr/bin/env bash
 CMD_WORKDIR=$(cd $(dirname $0) && pwd)
-
-echo $CMD_WORKDIR
+FILE_INSTALL_DIR_MK="${1}/install_dir.mk"
+FILE_ENV_LOCAL="${1}/environment.local"
 setup_wizz_help() {
   cli_name=${0##*/}
   echo -e "
 \e[90m$cli_name\e[0m
-\e[96mSetting up the installation path for build scripts and makefiles.\e[0m
-
-\e[96mUsage:\e[0m $cli_name \e[93m[project_folder]\e[0m
+\e[96mHelper to setup the other commands of the CLI.\e[0m
 "
-  exit 1
 }
 
-log_trace "project_folder = ${1}"
-echo -e "${LOG_TRACE} ${1}"
-touch "${1}/install_dir.mk"
-echo "INSTALLDIR = something"
 setup_wizz_help
+touch "${FILE_INSTALL_DIR_MK}"
+echo -n -e "\e[96mName of the project folder in the GEMDOS drive : \e[0m"
+read install_dir
+echo -e "INSTALLDIR = ${1}/build/${install_dir}" > "${FILE_INSTALL_DIR_MK}"
+echo -e "export GEMDOS_DRIVE=\"${1}/build\"" > "${FILE_ENV_LOCAL}"
+echo -e "export AUTORUN_FOLDER=\"${1}/build/${install_dir}\"" >> "${FILE_ENV_LOCAL}"
+
+[[ ! -d "${1}/build/${install_dir}" ]] && mkdir -p ${1}/build/${install_dir}
+
+echo -e "\e[92mDone\e[0m"
+cat ${FILE_INSTALL_DIR_MK}
+echo "---"
+cat ${FILE_ENV_LOCAL}
+
+. ${FILE_ENV_LOCAL}
