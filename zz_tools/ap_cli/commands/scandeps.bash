8b4156b2fd6659db18bd0d3deb3c12a41e86897d
David SPORN <sporniket.studio@gmail.com>
2022-05-27 08:26:36 +0200

    generate actual dependencies file for makefile

--- zz_tools/ap_cli/commands/scandeps.bash
+++ zz_tools/ap_cli/commands/scandeps.bash
@@ -4,6 +4,7 @@ DIR_COMMONS_MACROS="${DIR_BASE}/00_commons/macros"
 DIR_COMMONS_LIBS="${DIR_BASE}/00_commons/libs"
 DIR_APP_WRECKING_BALLS="${DIR_BASE}/10_wrecking_ball"
 DIR_APP_SHEET_EXTRACTOR="${DIR_BASE}/20_sheet_extractor"
+FILE_TARGET="dependencies.local.mk"
 
 scandeps_help() {
   cli_name=${0##*/}
@@ -41,19 +42,19 @@ scan_dependencies_deeply() {
 
 scandeps_help
 log_info "Scanning commons dependencies..."
-scan_dependencies_deeply "COMMONS_MACROS" "${DIR_COMMONS_MACROS}" "*.s"
-scan_dependencies_deeply "COMMONS_LIBS" "${DIR_COMMONS_LIBS}" "*.s"
+scan_dependencies_deeply "COMMONS_MACROS" "${DIR_COMMONS_MACROS}" "*.s" > "${FILE_TARGET}"
+scan_dependencies_deeply "COMMONS_LIBS" "${DIR_COMMONS_LIBS}" "*.s" >> "${FILE_TARGET}"
 
 log_info "Scanning ${DIR_APP_WRECKING_BALLS}..."
 cd "${DIR_APP_WRECKING_BALLS}"
-scan_dependencies "SRCS" "." "*.s"
-scan_dependencies_deeply "INCLUDES" "./includes" "*.s"
-scan_dependencies_deeply "ASSETS" "./assets" "*"
+scan_dependencies "SRCS" "." "*.s" > "${FILE_TARGET}"
+scan_dependencies_deeply "INCLUDES" "./includes" "*.s" >> "${FILE_TARGET}"
+scan_dependencies_deeply "ASSETS" "./assets" "*" >> "${FILE_TARGET}"
 cd ..
 
 log_info "Scanning ${DIR_APP_SHEET_EXTRACTOR}..."
 cd "${DIR_APP_SHEET_EXTRACTOR}"
-scan_dependencies "SRCS" "." "*.s"
-#scan_dependencies_deeply "INCLUDES" "./includes" "*.s"
-scan_dependencies_deeply "ASSETS" "./assets" "*"
+scan_dependencies "SRCS" "." "*.s" > "${FILE_TARGET}"
+#scan_dependencies_deeply "INCLUDES" "./includes" "*.s" >> "${FILE_TARGET}"
+scan_dependencies_deeply "ASSETS" "./assets" "*" >> "${FILE_TARGET}"
 cd ..
