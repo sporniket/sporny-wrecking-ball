6b0117d78b1149fc849f8a4388b401d77e28fc41
David SPORN <sporniket.studio@gmail.com>
2022-05-29 06:19:32 +0200

    completed all the build makefiles

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-SUBDIRS := 10_wrecking_ball 20_sheet_extractor
+SUBDIRS := 10_wrecking_ball  20_font_extractor  20_font_generator  20_level_generator  20_sheet_extractor  30_check_hardware
 
 all: $(SUBDIRS)
 
@@ -8,7 +8,15 @@ $(SUBDIRS):
 # aliases for the CLI
 swb: 10_wrecking_ball
 
+fontext: 20_font_extractor
+
+fontgen: 20_font_generator
+
+lvlgen: 20_level_generator
+
 sheetext: 20_sheet_extractor
+
+checkhw: 30_check_hardware
 
 # end of aliases
 .PHONY: all clean $(SUBDIRS)
--- zz_tools/ap_cli/commands/build.bash
+++ zz_tools/ap_cli/commands/build.bash
@@ -8,7 +8,11 @@ Essentially, an alias to 'make'
 \e[96mUsage:\e[0m ${cli_name} \e[93m[goal]\e[0m
 \e[96mGoals:\e[0m
   \e[93mswb\e[0m       The game program
+  \e[93mfontext\e[0m   The font sheet extractor.
+  \e[93mfontgen\e[0m   The font template image generator.
+  \e[93mlvlgen\e[0m    The level files generator.
   \e[93msheetext\e[0m  The sprite sheet extractor.
+  \e[93mcheckhw\e[0m   An hardware test program.
   \e[93mall\e[0m       makes all
   \e[93m*\e[0m         Help
 "
@@ -16,7 +20,7 @@ Essentially, an alias to 'make'
 }
 
 case "$1" in
-  swb|sheetext|all)
+  swb|fontext|fontgen|lvlgen|sheetext|checkhw|all)
     make "${1}"
     ;;
   *)
