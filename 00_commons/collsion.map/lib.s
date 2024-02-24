92084b3057d62e4ccb4675ffb0a1fdc53d2d8d86
David SPORN <sporniket.studio@gmail.com>
2024-02-24 07:32:53 +0100

    wip -- init file

---
+++ 00_commons/collsion.map/lib.s
@@ -0,0 +1,7 @@
+; ================================================================================================================
+; (C) 2020..2024 David SPORN
+; Distributed AS IS, in the hope that it will be useful, but WITHOUT ANY WARRANTY
+; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+; ================================================================================================================
+; Collision Map -- Library
+; ================================================================================================================
