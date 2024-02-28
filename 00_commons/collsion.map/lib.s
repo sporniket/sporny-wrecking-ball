31c466fbd7234eb42839d0b0ae3459ec8c2cfeb0
David SPORN <sporniket.studio@gmail.com>
2024-02-28 06:03:14 +0100

    wip -- declare buffers

--- 00_commons/collsion.map/lib.s
+++ 00_commons/collsion.map/lib.s
@@ -5,3 +5,12 @@
 ; ================================================================================================================
 ; Collision Map -- Library
 ; ================================================================================================================
+
+; memory buffer
+; const swb_clsnmp:u16[4752]
+swb_clsnmp                          ds.w    4752
+; LUTs
+; const swb_clsnmp_offsetbytes_grid_lines:u16[20] // = [354 + 44*8*i for i in range(0,20)]
+swb_clsnmp_offsetbytes_grid_lines   ds.w    20
+; const swb_clsnmp_offsetbytes_lines:u16[216] // = [1 + 44*i for i in range(0,216)]
+swb_clsnmp_offsetbytes_lines        ds.w    216
