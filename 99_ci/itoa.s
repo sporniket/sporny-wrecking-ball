4abb5e6543f4db2f5b29a89522363614c0ef28e9
David SPORN <sporniket.studio@gmail.com>
2024-07-23 23:06:29 +0200

    processed successfully 99_ci

--- 99_ci/itoa.s
+++ 99_ci/itoa.s
@@ -1,20 +1,20 @@
-; ================================================================================================================
-; (C) 2023 David SPORN
-; Distributed AS IS, in the hope that it will be useful, but WITHOUT ANY WARRANTY
-; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-; ================================================================================================================
-; Test suite for itoa.s'
-; ================================================================================================================
-; Before all
-;
+* ================================================================================================================
+* (C) 2023 David SPORN
+* Distributed AS IS, in the hope that it will be useful, but WITHOUT ANY WARRANTY
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* ================================================================================================================
+* Test suite for itoa.s'
+* ================================================================================================================
+* Before all
+
                         PrintNewLine
                         PrintHeader             ITOA_messHead
                         bra                     ITOA_test1
 ITOA_messHead           dc.b                    "itoa tests",0
                         even
 
-; ================================================================================================================
-; test factory : test enum
+* ================================================================================================================
+* test factory : test enum
 ITOA_makePrintHexFromByte macro
                         ; 1 - address register to use
                         ; 2 - data register to use
@@ -23,47 +23,47 @@ ITOA_makePrintHexFromByte macro
                         ; 5 - second address register to use for comparaison
                         ; ---
                         ; prepare
-                        ;
+
                         ; \1 : strbuf
-                        lea .strbuf_\@,\1
+                        lea                     .strbuf_\@,\1
                         ; \2 : value
-                        moveq #0,\2
-                        move.b \3,\2
+                        moveq                   #0,\2
+                        move.b                  \3,\2
                         ; ---
                         ; execute
-                        ;
+
                         jsrA_itoa_appHexUint8   \1,\2
                         ; ---
                         ; verify
-                        ;
-                        lea .strbuf_\@,\1
-                        lea .strexpected_\@,\5 
-.compare_\@             move.b  (\1)+,\2
-                        cmp.b   (\5)+,\2
-                        beq     .sameChar_\@
+
+                        lea                     .strbuf_\@,\1
+                        lea                     .strexpected_\@,\5
+.compare_\@             move.b                  (\1)+,\2
+                        cmp.b                   (\5)+,\2
+                        beq                     .sameChar_\@
                         ; ---
                         ; failed
-                        PrintFail       .messDesc_\@
-                        PrintContinue2  messGot,.strbuf_\@
-                        bra             .end_\@
-                        ; --- 
-                        ; repeat until end of string
-.sameChar_\@            tst.b   \2
-                        beq     .pass_\@
-                        bra     .compare_\@ ; next
+                        PrintFail               .messDesc_\@
+                        PrintContinue2          messGot,.strbuf_\@
+                        bra                     .end_\@
                         ; ---
-                        ; pass
-.pass_\@                PrintPass .messDesc_\@
-                        bra .end_\@
-.messDesc_\@            dc.b "appHexUint8(\3) = '",\4,"'",0
-.strbuf_\@              ds.b 20
-.strexpected_\@         dc.b \4,0
+                        ; repeat until end of string
+.sameChar_\@            tst.b                   \2
+                        beq                     .pass_\@
+                        bra                     .compare_\@             ; next
+                                                                        ; ---
+                                                                        ; pass
+.pass_\@                PrintPass               .messDesc_\@
+                        bra                     .end_\@
+.messDesc_\@            dc.b                    "appHexUint8(\3) = '",\4,"'",0
+.strbuf_\@              ds.b                    20
+.strexpected_\@         dc.b                    \4,0
                         even
 .end_\@                 nop
                         endm
 
-; ================================================================================================================
-; test 1 : get expected strings
+* ================================================================================================================
+* test 1 : get expected strings
 ITOA_test1
                         ITOA_makePrintHexFromByte a5,d5,#0,"$00",a4
                         ITOA_makePrintHexFromByte a5,d5,#1,"$01",a4
